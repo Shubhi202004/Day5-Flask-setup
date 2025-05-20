bba117e07b04eefeb835dd0bdda6c68df61fc370
Shubhangi Sahane <160036096+Shubhi202004@users.noreply.github.com>
2025-05-20 21:51:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        return render_template('submit.html', name=name)
+    return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
